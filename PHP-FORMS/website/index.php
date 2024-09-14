89435c4177633ee898652ac5c320757a22152805
anikmondol <anikmondol558363@gmail.com>
2024-09-14 23:19:59 +0600

    video number-74 start

---
+++ PHP-FORMS/website/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="container">
+    <?php
+    include('./header.php');
+    ?>
+
+  <h1 class="heading">Home Page</h1>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti ex excepturi veritatis facere sit exercitationem pariatur ducimus aperiam at harum nulla ipsum similique explicabo omnis quasi, labore dolor ut sequi.
+  Molestias assumenda nesciunt dignissimos voluptate sequi ad pariatur consequuntur ex porro reiciendis veniam nam harum beatae unde iusto inventore voluptatem, excepturi rem ut earum quisquam maiores cumque a! Quos, dolorum.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti ex excepturi veritatis facere sit exercitationem pariatur ducimus aperiam at harum nulla ipsum similique explicabo omnis quasi, labore dolor ut sequi.
+  Molestias assumenda nesciunt dignissimos voluptate sequi ad pariatur consequuntur ex porro reiciendis veniam nam harum beatae unde iusto inventore voluptatem, excepturi rem ut earum quisquam maiores cumque a! Quos, dolorum.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti ex excepturi veritatis facere sit exercitationem pariatur ducimus aperiam at harum nulla ipsum similique explicabo omnis quasi, labore dolor ut sequi.
+  Molestias assumenda nesciunt dignissimos voluptate sequi ad pariatur consequuntur ex porro reiciendis veniam nam harum beatae unde iusto inventore voluptatem, excepturi rem ut earum quisquam maiores cumque a! Quos, dolorum.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti ex excepturi veritatis facere sit exercitationem pariatur ducimus aperiam at harum nulla ipsum similique explicabo omnis quasi, labore dolor ut sequi.
+  Molestias assumenda nesciunt dignissimos voluptate sequi ad pariatur consequuntur ex porro reiciendis veniam nam harum beatae unde iusto inventore voluptatem, excepturi rem ut earum quisquam maiores cumque a! Quos, dolorum.</p>
+
+     <?php
+    include('./footer.php');
+    ?>
+    </section>
+</body>
+</html>
---
+++ die-exit.php
@@ -0,0 +1,21 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+echo $a - $b;
+echo "<br>";
+
+// echo exit("anik akon bacha acy");
+// echo die("first echo print");
+
+echo $a + $b;
+echo "<br>";
+
+echo $a * $b;
+echo "<br>";
+
+echo $a / $b;
+echo "<br>";
+
+?>
