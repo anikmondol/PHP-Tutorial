3794b5aa52b187af9478e8f487e2d165b5a304e8
anikmondol <anikmondol558363@gmail.com>
2024-10-02 15:32:37 +0600

    php tutorial video-1 start

--- FILES/52-read-file.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$file = "anik.text";
-$my_file = fopen($file, "r");
-echo fread($my_file, filesize($file));
-fclose($my_file);
-
-?>
--- PHP-FORMS/43-register.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_REQUEST['btn'])) {
-
-    foreach ($_REQUEST as $value) {
-        echo $value;
-        echo "<br>";
-    }
-}
